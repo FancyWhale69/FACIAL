122d3ecbf4d82687ba9cc07efffed9f6f70b25f8
Hazim Bukhari <80190998+FancyWhale69@users.noreply.github.com>
2022-11-28 19:34:28 +0300

    Added support for deep3d_pytorch

--- face_render/handle_netface.py
+++ face_render/handle_netface.py
@@ -4,9 +4,9 @@ from scipy.io import loadmat,savemat
 import glob
 from scipy.signal import savgol_filter
 import argparse
-
+from tqdm import tqdm
 parser = argparse.ArgumentParser(description='netface_setting')
-parser.add_argument('--param_folder', type=str, default='/content/FACIAL/video_preprocess/train1_deep3Dface')
+parser.add_argument('--param_folder', type=str, default='../video_preprocess/train1_deep3Dface')
 
 opt = parser.parse_args()
 
@@ -16,8 +16,15 @@ mat_path_list = sorted(glob.glob(os.path.join(param_folder, '*.mat')))
 len_mat = len(mat_path_list)
 faceshape = np.zeros((len_mat, 257),float)
 
-for i in range(1,len_mat+1):
-    faceshape[i-1,:] = loadmat(os.path.join(param_folder,str(i)+'.mat'))['coeff'][0,:]
+for i,name in tqdm(zip(range(1,len_mat+1), mat_path_list)):
+    mat_contents= loadmat(os.path.join(param_folder,name.split('/')[-1]))
+    coeff= np.concatenate((mat_contents['id'][0],
+                        mat_contents['exp'][0],
+                        mat_contents['tex'][0],
+                        mat_contents['angle'][0],
+                        mat_contents['gamma'][0],
+                        mat_contents['trans'][0]))
+    faceshape[i-1,:] = coeff
 
 
 frames_out_path = os.path.join(param_folder,'train1.npz')
